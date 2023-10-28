f382f2ae56c830e414aa2f98d77d9df6b093b709
prographicsten <prographicsten@gmail.com>
2023-10-28 12:56:20 +0600

    new codes add

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Toaster
+            position="top-left"
+            reverseOrder={false}
+            />
             <Footer></Footer>
         </div>
     );
--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import toast from "react-hot-toast";
 
 const CheckOut = () => {
+
+  const {user} = useContext(AuthContext);
+  // console.log(user);
+
   const service = useLoaderData();
-  const { title, _id } = service || {};
   // console.log(service);
+  const { _id, title, price, img } = service || {};
 
   const checkout = {
     backgroundImage: 'url(https://i.ibb.co/9TQvfjs/2.jpg)',
@@ -14,24 +21,55 @@ const CheckOut = () => {
     height: '300px',
   };
 
+  const handleBookService = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name?.value;
+    const date = form.date?.value;
+    const email = form.email?.value;
+    const amount = form.amount?.value;
+    const message = form.message?.value;
+
+    const order = {name, date, email, amount, message, img, service: title, service_id: _id};
+    console.log(order);
+
+    // post order
+    fetch('http://localhost:5000/bookings', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(order),
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId) {
+        toast.success('Service book Successfully completed!')
+      }
+    })
+  };
+
   return (
     <div>
       <div style={checkout} className="relative xl:rounded-xl" >
-        <div className="absolute flex md:px-10 lg:px-20 gap-5 items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) 100%) h-full w-full xl:rounded-xl">
-          <h2 className="text-center font-bold text-5xl text-white">Check Out :</h2>
-          <h2 className="text-center font-bold text-5xl text-[#FF3811]">{title}</h2>
+        <div className="absolute flex justify-center items-center md:justify-start md:px-10 lg:px-20 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) 100%) h-full w-full xl:rounded-xl">
+          <h2 className="text-center font-bold text-2xl md:text-4xl lg:text-5xl text-white">Check Out</h2>
         </div>
       </div>
 
-      <div className="py-32 lg:my-32 bg-[#F3F3F3] px-4 md:px-0">
-        <form className="md:w-10/12 mx-auto space-y-6">
+      <h2 className="text-center font-bold text-2xl md:text-4xl lg:text-5xl text-[#FF3811] my-10">Book : {title}</h2>
+      <div className="py-32 lg:mb-32 bg-[#F3F3F3] px-4 md:px-0">
+        
+
+        <form onSubmit={handleBookService} className="md:w-10/12 mx-auto space-y-6">
           <div className="md:flex md:justify-between gap-6 space-y-6 md:space-y-0">
-            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="text" name="firstName" id="" placeholder="First Name" />
-            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="text" name="lastName" id="" placeholder="Last Name" />
+            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="text" name="name" id="" defaultValue={user?.displayName} placeholder="Your Name" />
+            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="date" name="date" id="" placeholder="" />
           </div>
           <div className="md:flex md:justify-between gap-6 space-y-6 md:space-y-0">
-            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="text" name="phone" id="" placeholder="Your Phone" />
-            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="email" name="email" id="" placeholder="Your Email" />
+            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="email" name="email" id="" defaultValue={user?.email} placeholder="Your Email" />
+            <input className="w-full py-2 rounded-md px-4 focus:outline-none" type="text" name="amount" id="" defaultValue={`$ ${price}`} placeholder="Due Amount" />
           </div>
           <div>
             <textarea className="w-full py-2 rounded-md px-4 focus:outline-none" name="message" id="" cols="30" rows="6"></textarea>
