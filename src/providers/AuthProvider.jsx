dba6d1c156a1de08fdd27071f2a87fb655d6715f
prographicsten <prographicsten@gmail.com>
2023-10-31 18:54:29 +0600

    user cookie add and remove form loaclhost cookies in browser

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -32,13 +33,30 @@ const AuthProvider = ({children}) => {
     // on auth state change
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = { email: userEmail };
+
             setUser(currentUser);
+            console.log('current user is', currentUser);
             setLoading(false);
+
+            // if user is existing then issue a token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', loggedUser, {withCredentials: true})
+                .then(res => {
+                    console.log('token response', res.data);
+                })
+            }else{
+                axios.post('http://localhost:5000/logout', loggedUser, {withCredentials: true})
+                .then(res => {
+                    console.log(res.data);
+                })
+            }
         });
         return () => {
             return unsubscribe();
         }
-    }, [])
+    }, [user?.email])
 
     // all providers
     const authInfo = {
